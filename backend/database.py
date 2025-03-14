b98b413c4f34660d43d58d7efc6bcd283c964b18
LucasBorges21 <141840551+LucasBorges21@users.noreply.github.com>
2025-03-14 14:06:11 -0300

    Update database.py
    Adicionado as credenciais de acesso para o banco de dados AWS RDS e opções para erro

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,28 @@
 import mysql.connector
+from mysql.connector import Error
 
 def get_db_connection():
-    return mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='g25SIAN',
-        database='bd_iFood'
-    )
+    try:
+        conexao = mysql.connector.connect(
+            host='db-teste2.c1uka62e40t7.sa-east-1.rds.amazonaws.com',
+            user='admin',
+            password='teste-impacta',
+            database='teste_crud'
+        )
+
+        if conexao.is_connected():
+            print("✅ Conexão com AWS RDS estabelecida com sucesso!")
+            return conexao
+
+    except Error as e:
+        print(f"❌ Erro ao conectar com AWS RDS: {e}")
+        return None
+
+if __name__ == "__main__":
+    conn = get_db_connection()
+    if conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT DATABASE();")
+        db_atual = cursor.fetchone()
+        print(f"📚 Banco conectado: {db_atual[0]}")
+        conn.close()
